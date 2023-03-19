bedac772dc6eabac6f8b559940391e8b2fb5577e
Levent <srrlevent@gmail.com>
2023-03-19 22:46:11 +0300

    data has been linked to the required places on the detail screen.

--- app/src/main/java/com/leventsurer/rickandmorty/ui/fragments/CharacterDetailFragment.kt
+++ app/src/main/java/com/leventsurer/rickandmorty/ui/fragments/CharacterDetailFragment.kt
@@ -1,13 +1,21 @@
 package com.leventsurer.rickandmorty.ui.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.leventsurer.rickandmorty.data.model.CharacterDetailModel
+import com.leventsurer.rickandmorty.data.model.Location
+import com.leventsurer.rickandmorty.data.model.Origin
 import com.leventsurer.rickandmorty.databinding.FragmentCharacterDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
 
 @AndroidEntryPoint
 class CharacterDetailFragment : Fragment() {
@@ -32,7 +40,39 @@ class CharacterDetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         handleArguments()
-        binding.name.text = characterDetailModel.name
+        setupUi()
+        onClickHandler()
+    }
+
+    private fun onClickHandler() {
+        binding.apply {
+            iwBackButton.setOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setupUi() {
+        val origin = Origin(characterDetailModel.origin?.name).name
+        val location = Location(characterDetailModel.location?.name).name
+        val episodes  = arrayListOf<String>()
+        for(episode in characterDetailModel.episode){
+            episodes.add(episode.split("/").last())
+        }
+        val date = characterDetailModel.created?.let { modifyDateLayout(it) }
+
+        binding.apply {
+            twCharacterStatus.text = characterDetailModel.status
+            twCharacterName.text = characterDetailModel.name
+            twCharacterSpecy.text = characterDetailModel.species
+            twCharacterGender.text = characterDetailModel.gender
+            twCharacterOrigin.text = origin
+            twCharacterLocation.text = location
+            twCharacterEpisodes.text = episodes.toString()
+            twCharacterCreatedAt.text = "${date?.get(0)} , ${date?.get(1)}"
+            Glide.with(requireContext()).load(characterDetailModel.image).into(iwCharacterImage)
+        }
     }
 
     private fun handleArguments() {
@@ -42,6 +82,15 @@ class CharacterDetailFragment : Fragment() {
 
 
         }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    @Throws(ParseException::class)
+    private fun modifyDateLayout(date: String): List<String> {
+
+        val allDate = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(date)
+        val parsedDate = allDate.let { SimpleDateFormat("dd.MMM.yyyy HH:mm:ss").format(it) }
+        return parsedDate.split(" ")
     }
 
 
